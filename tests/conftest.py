2c7bc92e75beee9758695c797402b28ee1cf10ce
mrced <cedric.migazzi@hotmail.fr>
2019-02-14 13:04:23 +0100

    parser package created

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import json
+
 from grandpy import create_app
 
 
@@ -17,3 +19,42 @@ def app():
 def client(app):
     """A test client for the app."""
     return app.test_client()
+
+
+@pytest.fixture
+def questions():
+    return {
+        0: {"question": "Est-ce que tu connais l'adresse d'openclassrooms ?",
+            "words": ["est", "ce", "que", "tu", "connais", "l", "adresse",
+                      "d", "openclassrooms"],
+            "keywords": ["openclassrooms"]
+            },
+        1: {"question": "Connais-tu l'adresse d'Openclassrooms ?",
+            "words": ["connais", "tu", "l", "adresse", "d", "openclassrooms"],
+            "keywords": ["openclassrooms"]
+            },
+        2: {"question": "tu connais l'adresse d'Openclassrooms ?",
+            "words": ["tu", "connais", "l", "adresse", "d", "openclassrooms"],
+            "keywords": ["openclassrooms"]
+            },
+        3: {"question": "Ou se trouve l'arc de triomphe ?",
+            "words": ["ou", "se", "trouve", "l", "arc", "de", "triomphe"],
+            "keywords": ["arc", "triomphe"]
+            },
+        4: {"question": "Quelle est l'adresse de l'arc de triomphe ?",
+            "words": ["quelle", "est", "l", "adresse", "de", "l", "arc",
+                      "de", "triomphe"],
+            "keywords": ["arc", "triomphe"]
+            },
+        5: {"question": "Le stade de France, tu connais ?",
+            "words": ["le", "stade", "de", "france", "tu", "connais"],
+            "keywords": ["stade", "france"]
+            }
+    }
+
+
+@pytest.fixture
+def base_stop():
+    with open('grandpy/words-base.json', encoding='utf-8') as f:
+        words = json.load(f)
+    return words
--- tests/test_index.py
+++ tests/test_index.py
@@ -1,7 +1,5 @@
-import pytest
 
 
 def test_index(client):
     response = client.get('/')
     assert b"<title>GrandPy Bot</title>" in response.data
-
