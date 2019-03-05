969b43bddb2aeb3729a2f3cad047591fa62c6dc6
mrced <cedric.migazzi@hotmail.fr>
2019-03-05 11:49:42 +0100

    fixing footer display

--- grandpy/__init__.py
+++ grandpy/__init__.py
@@ -8,23 +8,12 @@ from flask import Flask
 G_API_KEY = os.environ.get("G_API_KEY")
 
 
-def create_app(test_config=None):
+def create_app():
     """Is factory function."""
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY='dev'
     )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
     from . import index
     app.register_blueprint(index.bp)
